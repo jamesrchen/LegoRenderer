df497587c1e4b5e22ec457517f3a8ddb36def92c
James Chen <unknownguy2002@gmail.com>
2022-01-26 22:37:35 +0800

    Add renderer

---
+++ renderer/clean.sh
@@ -0,0 +1,2 @@
+find ../output -type f ! -name 'envmap.exr' ! -name 'plane.obj' -delete
+find ../output -type d -delete
---
+++ renderer/constants.py
@@ -0,0 +1,6 @@
+PATH_MESHES = '../output'
+PATH_DATASET = '../input'
+PATH_UNIT_MESHES = '../unit_meshes'
+
+STR_LEGO_2_4 = 'lego_2_4.stl'
+STR_CUBE = 'cube.stl'
